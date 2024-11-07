06ca46d52c6fa70e9844f46dbcead1e9a2bfa7e3
iboonkhet <itservices@iddrives.co.th>
2024-11-07 11:51:24 +0700

    Update settings.py

--- trainingzenter/settings.py
+++ trainingzenter/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'app',
     'rest_framework',
     'rest_framework.authtoken',
-   
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
- 
+    'corsheaders.middleware.CorsMiddleware',
 
 ]
 X_FRAME_OPTIONS = 'SAMEORIGIN'  # แสดง Iframe
@@ -64,6 +64,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'https://pos.intnas.com',
+]
 
 
 TEMPLATES = [
