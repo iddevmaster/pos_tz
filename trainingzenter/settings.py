9b8051fca21239a6e3ec8f098473bfb548bf3240
iboonkhet <itservices@iddrives.co.th>
2024-11-07 10:13:25 +0700

    Update settings.py

--- trainingzenter/settings.py
+++ trainingzenter/settings.py
@@ -151,7 +151,7 @@ DEBUG = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/assets/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = '/trainingzenter/app/static'
