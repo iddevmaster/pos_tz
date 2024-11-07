b0ab14475664e122f1a876ca5e6e0b260f0d79c1
iboonkhet <itservices@iddrives.co.th>
2024-11-07 11:48:59 +0700

    Update settings.py

--- trainingzenter/settings.py
+++ trainingzenter/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'app',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders',
+   
 ]
 
 MIDDLEWARE = [
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+ 
 
 ]
 X_FRAME_OPTIONS = 'SAMEORIGIN'  # แสดง Iframe
