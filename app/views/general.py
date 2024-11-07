4ab074164dd7e706cf5c25ed789bb9f3324a8a29
iboonkhet <itservices@iddrives.co.th>
2024-11-07 11:38:30 +0700

    Update general.py

--- app/views/general.py
+++ app/views/general.py
@@ -16,6 +16,7 @@ def login(request):
 def login_check(request):
     username = request.POST['username']
     password = request.POST['password']
+    print("The original date is : " )
     user = auth.authenticate(username=username, password=password)
     if user is not None:
         auth.login(request, user)
