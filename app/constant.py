61779e45c8670fafb4ef520489f4cc2c51623b00
iboonkhet <itservices@iddrives.co.th>
2024-12-11 09:30:50 +0700

    Update constant.py

--- app/constant.py
+++ app/constant.py
@@ -1,4 +1,4 @@
-defaultTitle = "IDSS"
+defaultTitle = "IDSSs"
 webUrl = "http://127.0.0.1:8000"
 api_id_card = "https://localhost:8182/thaiid/read.jsonp?callback=callback&section1=true&section2a=true&section2c=true"
 key_module = ('tz', 'idss')
