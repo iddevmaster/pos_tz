ac5ae027aecc5b120e6c0cdc2e92c6a594ca29b2
Rkknoob <iboonkhet@gmail.com>
2025-02-19 15:14:37 +0700

    update
    update

--- app/urls.py
+++ app/urls.py
@@ -51,7 +51,7 @@ urlpatterns = [
     path('approve/update/processeventend',
          register.update_close_the_eventend, name="UpdateCloseEndTheEvent"),
     path('approve/update/event', register.approve_lis_event),
-    path('approvebill/update/bill', register.approve_list_payment),
+#     path('approvebill/update/bill', register.approve_list_payment),
     path('approvebill/update/bill2', register.approve_list_payment_update, name="UpdateCloseEndTheEventAll"
     ),
     path('approvebill/accept/<slug:pk>',
