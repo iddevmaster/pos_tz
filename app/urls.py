8d4b4a0fb74b6ef296351ffb8e7eb5c3bcd71e72
Rkknoob <iboonkhet@gmail.com>
2025-05-21 15:47:34 +0700

    update
    update

--- app/urls.py
+++ app/urls.py
@@ -196,9 +196,11 @@ urlpatterns = [
     path('teacher/print/all/overdue/<str:year>/<str:m>', report_and_print.register_report_summary_print_overdue),
     path('teacher/print/overdue/<str:teacher_id>/<str:year>/<str:m>', report_and_print.register_report_summary_print_overdue_one),
 
+
+
     path('report/withdraw/all/summary/teacher', report_and_print.register_report_summary_teacher),
     path('report/withdraw/all/withdraw', report_and_print.register_report_summary_teacher_withdraw),
- 
+    path('report/withdraw/all/summary/teacher/<str:year>/<str:m>', report_and_print.register_report_summary_teacher_all),
 
     path('report/register/export/learning_status',
          report_and_print.register_report_learn_status),
