e5cfebaa845916cfecfc985a649957f64789dd18
Rkknoob <iboonkhet@gmail.com>
2025-05-24 15:12:19 +0700

    Update course.py

--- app/views/course.py
+++ app/views/course.py
@@ -914,9 +914,9 @@ def calendar_event_api2(request,id):
         te.append(fs)  
         
         
-       nextdayend = addDay(1, int(y), int(m), int(d))
+       nextdayend = addDay(0, int(y), int(m), int(d))
        result = course.objects.filter(course_id=r.ev.course_id).first()
-     
+       
        
        pay = pay_item.objects.filter(id=r.pi_id).first()
        
