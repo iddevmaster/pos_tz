d57cd5a8745a634f005430f7e0bdd73688c9d308
Rkknoob <iboonkhet@gmail.com>
2025-06-11 17:36:31 +0700

    Update settings.py

--- trainingzenter/settings.py
+++ trainingzenter/settings.py
@@ -66,9 +66,9 @@ REST_FRAMEWORK = {
 }
 
 
-CSRF_TRUSTED_ORIGINS = ["https://pos.trainingzenter.com"]
-CSRF_ALLOWED_ORIGINS = ["https://pos.trainingzenter.com"]
-CORS_ORIGINS_WHITELIST = ["https://pos.trainingzenter.com"]
+CSRF_TRUSTED_ORIGINS = ["https://idss.idvocationalskilltrainingschool.com"]
+CSRF_ALLOWED_ORIGINS = ["https://idss.idvocationalskilltrainingschool.com"]
+CORS_ORIGINS_WHITELIST = ["https://idss.idvocationalskilltrainingschool.com"]
 
 
 TEMPLATES = [
