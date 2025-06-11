14bbc3098c034177b8f7329b4b0e378f04062d30
Rkknoob <iboonkhet@gmail.com>
2025-06-11 15:28:04 +0700

    Update settings.py

--- trainingzenter/settings.py
+++ trainingzenter/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'trainingzenter.wsgi.application'
 DATABASES = {
        'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'backup_idss',
+        'NAME': 'idss',
         'USER': 'root',
         'HOST': '27.254.149.247',
         'PASSWORD': '@P@SS.W0rd',
