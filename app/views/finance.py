63b12ab4ad7149c8bb6903ee1552ef0e88a14be1
iddevmaster <itservices@iddrives.co.th>
2025-07-04 13:57:57 +0700

    update
    update

--- app/views/finance.py
+++ app/views/finance.py
@@ -6,11 +6,12 @@ from django.contrib.auth.models import auth
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count, Sum, Value
 from django.db.models.functions import TruncMonth
-from ..models import category_program_permission, course, course_event, teacher_income_setting, billing_cycle_setting, user_group, user_detail, teacher,pay_item,compensation,event_register,salesorder,register_main,location_thai,document,fact_teacher_user,register_payment,condition,conhead,tax_setting,fact_commission,commissionstages
+from ..models import category_program_permission, course, course_event, teacher_income_setting, billing_cycle_setting, user_group, user_detail, teacher,pay_item,compensation,event_register,salesorder,register_main,location_thai,document,fact_teacher_user,register_payment,condition,conhead,tax_setting,fact_commission,commissionstages,register_payment_items
 from ..constant import defaultTitle, thai_months,unitPayChoices
 from ..functions import dateTimeNow, last_day_of_month
 from ..forms.finance_form import billing_cycle_setting_form
 from ..forms.teacher_form import teacherIncomeSettingForm
+from decimal import Decimal
 from django.http import JsonResponse
 import json
 from django.http import JsonResponse
@@ -1177,13 +1178,26 @@ def withdraw_list_one_com(request):
     for r in commit:
        
         content_main = register_main.objects.select_related("course","ev").get(register_id=r.register_id)
+        paymet = register_payment_items.objects.get(register_id=r.register_id)
+        
         stage = commissionstages.objects.get(stage_id=r.stage_id)
-        r = {'stage_name':stage.stage_name,'commission_rate':stage.commission_rate,"couse_name":content_main.course.course_name,"ev_date_start":content_main.ev.ev_date_start,"ev_date_end":content_main.ev.ev_date_end}
+
+
+        #### คำนวน ####
+        getpricebill = paymet.rpi_price_result
+        getcom = ((stage.commission_rate) / 100 )
+        print(getpricebill)
+        print(getcom)
+        total = Decimal(getpricebill) * Decimal(getcom)
+        
+        r = {'rpi_price_result':paymet.rpi_price_result,'register_number':content_main.register_number,"course_name":content_main.course.course_name,'ev_generation':content_main.ev.ev_generation,'stage_name':stage.stage_name,'commission_rate':stage.commission_rate,"couse_name":content_main.course.course_name,"ev_date_start":content_main.ev.ev_date_start,"ev_date_end":content_main.ev.ev_date_end,'price_com':total}
         obj.append(r)
 
-    print(obj)
-    context = {'title': title,'listMenuPermission': objMenu,'data':obj}
-    return render(request, 'finance/teachers_withdraw.html',context)
+        print(obj)
+
+   
+    context = {'title': title,'listMenuPermission': objMenu,'data':obj,'user_id':user_id}
+    return render(request, 'finance/commit_withdraw.html',context)
 
 
 
@@ -1399,4 +1413,11 @@ def withdraw_list_one(request):
 
 
 
+@csrf_exempt
+def sendwithdrawcom(request):
+   
+    data = json.loads(request.body)
+    user_id = data.get("user_id")
   
+    datas = {'status':user_id}
+    return JsonResponse(datas, status=200,safe=False)
