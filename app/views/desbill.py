3db4e61480dc563cfc16a93c1f3f5903fd500e88
iddevmaster <itservices@iddrives.co.th>
2025-08-04 15:15:32 +0700

    update
    update

--- app/views/desbill.py
+++ app/views/desbill.py
@@ -402,8 +402,10 @@ def data_bill(request):
     obj = []
 
       
-    content = register_payment_items.objects.select_related("rp","register").filter(register__ev_id=ev_id).order_by("-rp_id")
+    content = register_main.objects.filter(ev_id=ev_id,orderstatus='Deposit')
     for r in content:
+        
+       
         customer_list = customers.objects.select_related('register').filter(
             register_id=r.register_id).first()
         
@@ -418,22 +420,22 @@ def data_bill(request):
         # course_list = course_event.objects.select_related(
         #     'course').filter(ev_id=r.ev_id).first()
         ct = '-'
-        if r.register.customer_type == '1':
+        if r.customer_type == '1':
             ct = 'เครดิต'
         else:
             ct = 'เงินสด'
 
         cus_type = '-'
-        if r.register.customer_type == '1':
+        if r.customer_type == '1':
             cus_type = 'บริษัท'
         else:
             cus_type = 'บุคคล'    
 
 
-        uuid_without_dashes = str(r.register.register_id).replace('-', '')
+        uuid_without_dashes = str(r.register_id).replace('-', '')
       
 
-        res = {'register_id':uuid_without_dashes,'ev_id':ev_id,'register_number': r.register.register_number,'pay_type':ct,'rp_name_seller':payment.rp_name_seller,'rp_name_customer':payment.rp_name_customer,'rpi_code':payment_i.rpi_code,'rpi_name':payment_i.rpi_name,'customer_type':cus_type,'rp_doc_number':payment.rp_doc_number}
+        res = {'register_id':uuid_without_dashes,'ev_id':ev_id,'register_number': r.register_number,'pay_type':ct,'rp_name_seller':payment.rp_name_seller,'rp_name_customer':payment.rp_name_customer,'rpi_code':payment_i.rpi_code,'rpi_name':payment_i.rpi_name,'customer_type':cus_type,'rp_doc_number':payment.rp_doc_number}
         obj.append(res)
 
 
