309bc42653ad2fb34492e7197e8a11218ddc58ec
GuPune <92207039+GuPune@users.noreply.github.com>
2025-08-07 17:15:50 +0700

    Update course.py

--- app/views/course.py
+++ app/views/course.py
@@ -886,11 +886,10 @@ def calendar_event_apiall(request):
     #  uuid_with_dashes = t1.teacher_id  # This is a UUID object
     #     uuid_without_dashes = str(uuid_with_dashes).replace('-', '')
         delta = r.ev_date_end - r.ev_date_start
-        days_difference = delta.days + 1
+
      
-        res = {'backgroundColor':t,'borderColor':'#1e7e34','textColor':'#ffffff','title': str(r.course.course_name) + " (รุ่นที่ " + str(r.ev_generation)+")" ,'data':sff,
-        'address':"สถานที่จัด จ."+str(location.province_name)+" อ."+str(location.amphur_name)+" ที่อยู่ "+str(r.address),
-                'limit_price_workhelp':r.limit_price_workhelp,'limit_price':r.limit_price,'dis_limit': r.limit_price - (teacher_income_setting.objects.filter(ev=r.ev_id,pi=3).aggregate(Sum('tis_sum'))['tis_sum__sum'] or 0), 'start': r.ev_date_start, 'end': dmytoymd(nextdayend),'evs_id':r.ev_id,'ev_hour':r.ev_hour,'ev_hour_two':r.ev_hour_two,'ev_hour_three':r.ev_hour_three,'ev_people': r.ev_people,'ev_people_two': r.ev_people_two,'ev_people_three': r.ev_people_three,'count_day':days_difference}
+        res = {'backgroundColor':t,'borderColor':'#1e7e34','textColor':'#ffffff','title': str(r.course.course_code) + " (Reg " + str(r.ev_generation)+")" ,'data':sff,
+        'address':"สถานที่จัด จ."+str(location.province_name)+" อ."+str(location.amphur_name)+" ที่อยู่ "+str(r.address),'start': r.ev_date_start, 'end': dmytoymd(nextdayend),'evs_id':r.ev_id,'ev_hour':r.ev_hour,'ev_hour_two':r.ev_hour_two,'ev_hour_three':r.ev_hour_three,'ev_people': r.ev_people,'ev_people_two': r.ev_people_two}
         obj.append(res)
        
     return JsonResponse(obj, safe=False)
