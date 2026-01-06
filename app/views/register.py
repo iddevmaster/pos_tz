a8c02c7d7a396be4771659c9ba3c48578c01a41d
iddevmaster <itservices@iddrives.co.th>
2026-01-06 22:25:05 +0700

    update
    update

--- app/views/register.py
+++ app/views/register.py
@@ -2400,10 +2400,11 @@ def approve_list_invoice_com(request):
         billpa = register_payment.objects.select_related('register').filter(register__close_the_sale=1,register__pay_type=2).order_by("-crt_date")[:30]
         for r in billpa:       
             erv = event_register.objects.get(register=r.register.register_id)
-        
-            getsale = salesorder.objects.get(er_id=erv.er_id)
+            evcourse = course_event.objects.get(ev_id=erv.ev.ev_id)
             
-            res = {'rp_doc_number':r.rp_doc_number,'register_number':r.register.register_number,'po':getsale.po,'sq':getsale.sq,'so':getsale.so,'register_id':r.register.register_id,'sale_id':getsale.sale_id,'invoice':getsale.invoice,'rv':getsale.rv,'status':getsale.status}
+
+            getsale = salesorder.objects.get(er_id=erv.er_id)
+            res = {'rp_doc_number':r.rp_doc_number,'register_number':r.register.register_number,'po':getsale.po,'sq':getsale.sq,'so':getsale.so,'register_id':r.register.register_id,'sale_id':getsale.sale_id,'invoice':getsale.invoice,'rv':getsale.rv,'status':getsale.status,'custom':r.rp_name_customer,'course_name':evcourse.course.course_name,'start':evcourse.ev_date_start,'end':evcourse.ev_date_end,'ev_generation':evcourse.ev_generation}
 
             obj.append(res)    
            
