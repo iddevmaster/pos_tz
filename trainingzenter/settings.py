63f96695184fb8d9a62665a85a11768b6d5f8779
iddevmaster <itservices@iddrives.co.th>
2026-02-02 09:21:17 +0700

    update
    update

--- trainingzenter/settings.py
+++ trainingzenter/settings.py
@@ -111,9 +111,9 @@ WSGI_APPLICATION = 'trainingzenter.wsgi.application'
 DATABASES = {
        'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'postz',
+        'NAME': 'idss_new',
         'USER': 'root',
-        'HOST': '27.254.149.244',
+        'HOST': '27.254.149.247',
         'PASSWORD': '@P@SS.W0rd',
         'PORT': '3306',
         'OPTIONS': {
