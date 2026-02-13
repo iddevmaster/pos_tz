4f6126fe0d709eea1c0f88eb457efb5a5ecdcc60
iddevmaster <itservices@iddrives.co.th>
2026-02-13 11:27:49 +0700

    Update finance.py

--- app/views/finance.py
+++ app/views/finance.py
@@ -1266,10 +1266,10 @@ def withdraw_list_one_com(request):
         r = {'rpi_price_result':paymet.rpi_price_result,'register_number':content_main.register_number,"course_name":content_main.course.course_name,'ev_generation':content_main.ev.ev_generation,'stage_name':stage.stage_name,'commission_rate':stage.commission_rate,"couse_name":content_main.course.course_name,"ev_date_start":content_main.ev.ev_date_start,"ev_date_end":content_main.ev.ev_date_end,'price_com':total,'getpricebill_after_vat':getpricebill_after_vat,'after_vat_cal':after_vat_cal}
         obj.append(r)
 
-        
-        dec = decimal.Decimal(total_all)
+        d = Decimal(total_all)
+        s = '{0:f}'.format(d)
 
-        total_all = str(dec)
+        print(s)
  
         
     context = {'title': title,'listMenuPermission': objMenu,'data':obj,'user_id':user_id,'total_all':total_all}
