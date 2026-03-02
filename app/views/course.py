b8d6524907fc0a5c7dfefcb6b9c25aa31daf1039
iddevmaster <itservices@iddrives.co.th>
2026-03-02 15:43:34 +0700

    ียกฟะำ

--- app/views/course.py
+++ app/views/course.py
@@ -833,11 +833,13 @@ def calendar_event_apiall(request):
     start = request.GET.get('start', None)
     end = request.GET.get('end', None)
     _date = date.today()
+    a = date(2026, 1, 1)
+    b = date(2026, 12, 31)
     if start is not None and end is not None:
         # 2022-10-31T00:00:00+07:00 to  2022-10-31
-        sobj = str(start).split("T")[0]
+        sobj = a
         # 2022-10-31T00:00:00+07:00 to  2022-10-31
-        eobj = str(end).split("T")[0]
+        eobj = b
     else:
         sobj = _date + timedelta(days=0)
         eobj = _date + timedelta(days=60)
